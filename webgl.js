b85b8c424ffec1ea6c0c7540f2d74ad96ec9c946
Aaron McLeod <aaron@uken.com>
2016-02-01 13:25:35 -0500

    working on batch drawing.

--- webgl.js
+++ webgl.js
@@ -1,79 +1,64 @@
-var Webgl = {
-  initialize: function(canvas) {
-    this.canvas = canvas;
-    this.gl = canvas.getContext('experimental-webgl');
-  },
+(function() {
+  var MAX_QUADS_PER_BATCH = 100; // arbitrary for now
+  var VERTS_PER_QUAD = 4;
+  var INDICES_PER_QUAD = 6;
+  var FLOATS_PER_VERT = 2;
+  var Webgl = {
+    initialize: function(canvas) {
+      this.canvas = canvas;
+      this.gl = canvas.getContext('experimental-webgl');
+      this.initArrays();
+    },
 
-  createBuffers: function() {
-    this.positionBuffer = this.gl.createBuffer();
-    this.textureBuffer = this.gl.createBuffer();
-    this.indexBuffer = this.gl.createBuffer();
-  },
+    createBuffers: function() {
+      this.positionBuffer = this.gl.createBuffer();
+      this.textureBuffer = this.gl.createBuffer();
+      this.indexBuffer = this.gl.createBuffer();
+    },
 
-  buildTexture: function(image) {
-    var gl = this.gl;
-    var texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.bindTexture(gl.TEXTURE_2D, null);
+    buildTexture: function(image) {
+      var gl = this.gl;
+      var texture = gl.createTexture();
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+      gl.bindTexture(gl.TEXTURE_2D, null);
 
-    return texture;
-  },
+      return texture;
+    },
 
-  compileShaders: function(vert, frag) {
-    var gl = this.gl;
-    this.shader = new Shader(gl, vert, frag);
-    var shader = this.shader;
-    shader.vertexPositionAttribute = gl.getAttribLocation(shader.shaderProgram, "aVertexPosition");
-    gl.enableVertexAttribArray(shader.vertexPositionAttribute);
+    compileShaders: function(vert, frag) {
+      var gl = this.gl;
+      this.shader = new Shader(gl, vert, frag);
+      var shader = this.shader;
+      shader.vertexPositionAttribute = gl.getAttribLocation(shader.shaderProgram, "aVertexPosition");
+      gl.enableVertexAttribArray(shader.vertexPositionAttribute);
 
-    shader.textureCoordAttribute = gl.getAttribLocation(shader.shaderProgram, "aTextureCoord");
+      shader.textureCoordAttribute = gl.getAttribLocation(shader.shaderProgram, "aTextureCoord");
 
-    gl.enableVertexAttribArray(shader.textureCoordAttribute);
+      gl.enableVertexAttribArray(shader.textureCoordAttribute);
 
-    shader.samplerUniform = gl.getUniformLocation(shader.shaderProgram, "uSampler");
-  },
+      shader.samplerUniform = gl.getUniformLocation(shader.shaderProgram, "uSampler");
+    },
 
-  draw: function() {
-    var gl = this.gl;
-
-    gl.viewport(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    var shader = this.shader;
-
-    var matrixLocation = gl.getUniformLocation(shader.shaderProgram, "uMatrix");
-
-    gl.activeTexture(gl.TEXTURE0);
-    var atlas = this.atlas;
-    gl.bindTexture(gl.TEXTURE_2D, atlas.texture);
-
-    for (var i = 0; i < this.gallery.galleryImages.length; i++) {
-      mat3.identity(this.mvMatrix);
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
-
-      var galleryImage = this.gallery.galleryImages[i];
-      var x1 = galleryImage.position.x;
-      var y1 = galleryImage.position.y;
-
-      var region = atlas.regions[galleryImage.regionName];
-
+    addQuad: function(x1, y1, atlas, region) {
+      var vertexOffset = this.quadCount * VERTS_PER_QUAD * FLOATS_PER_VERT;
       var x2 = x1 + region.w;
       var y2 = y1 + region.h;
-      var vertices = [
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x2, y2
-      ];
+      this.verts[vertexOffset] = x1, this.verts[vertexOffset + 1] = y1;
+      this.verts[vertexOffset + 2] = x2, this.verts[vertexOffset + 3] = y1;
+      this.verts[vertexOffset + 4] = x1, this.verts[vertexOffset + 5] = y2;
+      this.verts[vertexOffset + 6] = x2, this.verts[vertexOffset + 7] = y2;
 
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-      gl.vertexAttribPointer(shader.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.textureBuffer);
+      var indiceOffset = this.quadCount * INDICES_PER_QUAD;
+      this.indices[indiceOffset] = vertexOffset;
+      this.indices[indiceOffset + 1] = vertexOffset + 1;
+      this.indices[indiceOffset + 2] = vertexOffset + 2;
+      this.indices[indiceOffset + 3] = vertexOffset + 2;
+      this.indices[indiceOffset + 4] = vertexOffset + 3;
+      this.indices[indiceOffset + 5] = vertexOffset + 1;
 
       var sx = region.x;
       var sy = region.y;
@@ -84,14 +69,33 @@ var Webgl = {
       var ty1 = 1.0 - (sy / atlas.image.height);
       var tx2 = ((sx + sw) / atlas.image.width);
       var ty2 = 1.0 - ((sy + sh) / atlas.image.height);
-      var textureCoords = [
-        tx1, ty1,
-        tx2, ty1,
-        tx1, ty2,
-        tx2, ty2
-      ];
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-      gl.vertexAttribPointer(shader.textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+      this.textureCoords[vertexOffset] = tx1, this.textureCoords[vertexOffset + 1] = ty1;
+      this.textureCoords[vertexOffset + 2] = tx2, this.textureCoords[vertexOffset + 3] = ty1;
+      this.textureCoords[vertexOffset + 4] = tx1, this.textureCoords[vertexOffset + 5] = ty2;
+      this.textureCoords[vertexOffset + 6] = tx2, this.textureCoords[vertexOffset + 7] = ty2;
+
+      this.quadCount++;
+
+      if (this.quadCount >= MAX_QUADS_PER_BATCH) {
+        this.flushQuads();
+      }
+    },
+
+    draw: function() {
+      var gl = this.gl;
+
+      gl.viewport(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+      gl.clearColor(0, 0, 0, 1);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+      var shader = this.shader;
+
+      var matrixLocation = gl.getUniformLocation(shader.shaderProgram, "uMatrix");
+
+      gl.activeTexture(gl.TEXTURE0);
+      var atlas = this.atlas;
+      gl.bindTexture(gl.TEXTURE_2D, atlas.texture);
+      mat3.identity(this.mvMatrix);
 
       mat3.multiply(this.mvMatrix, this.mvMatrix, [
         2 / this.canvas.clientWidth, 0, 0,
@@ -102,27 +106,59 @@ var Webgl = {
       gl.uniformMatrix3fv(matrixLocation, false, this.mvMatrix);
       gl.uniform1i(shader.samplerUniform, 0);
 
+      for (var i = 0; i < this.gallery.galleryImages.length; i++) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+
+        var galleryImage = this.gallery.galleryImages[i];
+        var x1 = galleryImage.position.x;
+        var y1 = galleryImage.position.y;
+
+        var region = atlas.regions[galleryImage.regionName];
+        this.addQuad(x1, y1, atlas, region);
+      }
+
+      if (this.quadCount > 0) {
+        this.flushQuads();
+      }
+
+      requestAnimationFrame(Webgl.draw);
+    },
+
+    flushQuads: function() {
+      var gl = this.gl;
+      var shader = this.shader;
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, this.verts, gl.STATIC_DRAW);
+      gl.vertexAttribPointer(shader.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.textureBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, this.textureCoords, gl.STATIC_DRAW);
+      gl.vertexAttribPointer(shader.textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
 
-      var indexes = [
-        0, 1, 2, 2, 3, 1
-      ];
+      gl.drawElements(gl.TRIANGLES, INDICES_PER_QUAD * MAX_QUADS_PER_BATCH, gl.UNSIGNED_SHORT, 0);
 
-      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexes), gl.STATIC_DRAW);
+      this.initArrays();
+    },
 
-      gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+    initArrays: function() {
+      this.verts = new Float32Array(MAX_QUADS_PER_BATCH * VERTS_PER_QUAD * FLOATS_PER_VERT);
+      this.textureCoords = new Float32Array(MAX_QUADS_PER_BATCH * VERTS_PER_QUAD * FLOATS_PER_VERT);
+      this.indices = new Float32Array(MAX_QUADS_PER_BATCH * INDICES_PER_QUAD);
+      this.quadCount = 0;
+    },
+
+    setupGL: function(atlas) {
+      var gl = this.gl;
+      gl.enable(gl.DEPTH_TEST);
+      gl.enable(gl.BLEND);
+      gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+      this.mvMatrix = mat3.create();
+      atlas.texture = this.buildTexture(atlas.image);
     }
-
-    requestAnimationFrame(Webgl.draw);
-  },
-
-  setupGL: function(atlas) {
-    var gl = this.gl;
-    gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-
-    this.mvMatrix = mat3.create();
-    atlas.texture = this.buildTexture(atlas.image);
-  }
-};
+  };
+  window.Webgl = Webgl;
+}).call(this);
