f6a108cf05e9ab718951c1cfb563e72266fc9d62
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-07-12 23:24:43 -0400

    explain w

--- vert.glsl
+++ vert.glsl
@@ -9,6 +9,9 @@ varying vec2 vTextureCoord;
 void main() {
   // take our final matrix to modify the vertex position to display the data on screen in a perspective way
   // With shader code here, you can modify the look of an image in all sorts of ways
+  // the 4th value here is the w coordinate, and it is called Homogeneous coordinates, (x,y,z,w).
+  // It effectively allows the perspective math to work. With 3d graphics, it should be set to 1. Less than 1 will appear too big
+  // Greater than 1 will appear too small
   gl_Position = uMatrix * vec4(aVertexPosition, 1);
   vTextureCoord = aTextureCoord;
 }
